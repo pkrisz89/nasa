072e001f838b97a292a703c0b0b2394c2fbeb202
Kris Pinter <pinter.krisztian89@gmail.com>
2017-09-07 20:43:27 +0100

    handle audio

--- src/utils/mapItems.js
+++ src/utils/mapItems.js
@@ -2,6 +2,8 @@ import React from 'react';
 import LazyLoad from 'react-lazyload';
 import styled from 'styled-components';
 
+const audioFile = '/img/audio.png';
+
 const ImageWrapper = styled.div`
     margin: 10px;
     height: 180px;
@@ -15,15 +17,25 @@ const Image = styled.img`
 
 export default function(items) {
     if (items) {
-      return items.map(item => {
-        return (
-            <ImageWrapper key={item.data[0].nasa_id}>
-                <LazyLoad once>
-                    <Image src={item.links[0].href} alt=""/>
-                </LazyLoad>
-                {item.data[0].title}
-            </ImageWrapper>
+       return items.map(item => {
+            if (item.data[0].media_type === 'audio') {
+                return (
+                    <ImageWrapper key={item.data[0].nasa_id}>
+                        <LazyLoad once>
+                            <Image src={audioFile} alt=""/>
+                        </LazyLoad>
+                        {item.data[0].title}
+                    </ImageWrapper>
+                )
+            }
+            return (
+                <ImageWrapper key={item.data[0].nasa_id}>
+                    <LazyLoad once>
+                        <Image src={item.links[0].href} alt=""/>
+                    </LazyLoad>
+                    {item.data[0].title}
+                </ImageWrapper>
             )
-      })
+        })
     }
-  };
+};
